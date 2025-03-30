655bc9a0592239b4164ed4c5fbabfa710dc522f4
Abdullah Bin Omar Chowdhury <abdullahbinomarchowdhury@gmail.com>
2025-03-30 12:44:50 +0600

    feature(header): fix avatar size

--- src/components/ui/header/Avatar.tsx
+++ src/components/ui/header/Avatar.tsx
@@ -1,58 +1,3 @@
-// import { FC, useState } from "react";
-
-// interface User {
-//   name: string;
-//   avatarUrl?: string;
-// }
-
-// const Avatar: FC = () => {
-//   const user: User = {
-//     name: "Jane Doe",
-//     avatarUrl:
-//       "https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fHww", // Using placeholder image
-//   };
-//   const fallbackAvatarUrl = "https://via.placeholder.com/150?text=No+Avatar";
-//   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-//   // Toggle dropdown visibility
-//   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
-
-//   return (
-//     <div className="relative">
-//       <div onClick={toggleDropdown}>
-//         <img
-//           className="w-[45px] h-[45px] rounded-[50%] object-cover"
-//           src={user.avatarUrl || fallbackAvatarUrl} // Use fallback if avatarUrl is not provided
-//           alt={user.name}
-//         />
-//       </div>
-//       {isDropdownOpen && (
-//         <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md p-3">
-//           <div className="pb-2 border-b border-gray-200">
-//             <h1 className="font-bold text-gray-800">{user.name}</h1>
-//           </div>
-//           <ul className="text-sm font-bold mt-2">
-//             <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer rounded-md">
-//               Profile
-//             </li>
-//             <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer rounded-md">
-//               Theme
-//             </li>
-//             <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer rounded-md">
-//               Sign Out
-//             </li>
-//             <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer rounded-md">
-//               Feedback
-//             </li>
-//           </ul>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Avatar;
-
 import { FC, useState, useEffect, useRef } from "react";
 
 interface User {
@@ -93,7 +38,7 @@ const Avatar: FC = () => {
     <div className="relative" ref={dropdownRef}>
       <div onClick={toggleDropdown}>
         <img
-          className="w-[45px] h-[45px] rounded-full object-cover cursor-pointer"
+          className="w-[40px] h-[40px] rounded-full object-cover cursor-pointer"
           src={user.avatarUrl || fallbackAvatarUrl}
           alt={user.name}
         />
