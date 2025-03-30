77f80e060b867112a4f1121af4f94d01a96638e2
Abdullah Bin Omar Chowdhury <abdullahbinomarchowdhury@gmail.com>
2025-03-30 12:48:27 +0600

    feature(header): fix header padding

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,7 @@ import Avatar from "../ui/header/Avatar";
 const Header: FC = () => {
   return (
     <section>
-      <div className="w-full px-2 py-4">
+      <div className="w-full px-2 py-2">
         <div className="flex items-center justify-between">
           <div className="">
             <div className="flex justify-start items-center space-x-2">
