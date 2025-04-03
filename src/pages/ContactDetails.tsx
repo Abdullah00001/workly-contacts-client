ee6d7c364941a494c431c033b46fc7179c50f67f
Abdullah Bin Omar Chowdhury <abdullahbinomarchowdhury@gmail.com>
2025-04-03 15:21:41 +0600

    feature(contact-details-page): page is now responsive for all devices

--- src/pages/ContactDetails.tsx
+++ src/pages/ContactDetails.tsx
@@ -28,7 +28,7 @@ const ContactDetails: FC = () => {
   };
   return (
     <>
-      <div className="w-full">
+      <div className="w-full lg:w-full xl:w-[950px] lg:p-8">
         <div className="flex justify-between items-center">
           {/* Left Arrow Icon */}
           <div onClick={handleReturn} className="p-2 cursor-pointer">
@@ -56,8 +56,8 @@ const ContactDetails: FC = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center mt-[50px] space-y-7">
-          <div className="w-[112px] h-[112px] cursor-pointer">
+        <div className="flex flex-col justify-center items-center lg:flex-row lg:justify-start lg:items-center lg:space-x-10 mt-[50px] space-y-7">
+          <div className="w-[112px] h-[112px] md:w-[268px] md:h-[268px] lg:w-[162px] lg:h-[162px] cursor-pointer">
             <img
               src={`https://api.dicebear.com/7.x/initials/svg?seed=Abdullah`}
               alt="Avatar"
@@ -65,58 +65,62 @@ const ContactDetails: FC = () => {
             />
           </div>
           <div>
-            <h1 className="text-center text-wrap text-2xl">
+            <h1 className="text-center font-normal text-wrap text-2xl lg:text-[28px]">
               Abdullah Bin Omar Chowdhury
             </h1>
           </div>
         </div>
-        <div className="flex flex-col justify-center mt-5">
-          <div className="flex flex-col justify-start space-y-4  p-4 bg-[#f0f4f9] rounded-[12px] text-[#1f1f1f]">
+        <div className="flex flex-col justify-start lg:justify-start lg:flex-row lg:space-x-4 lg:items-start">
+          <div className="flex flex-col justify-center mt-5">
+            <div className="flex flex-col justify-start space-y-4  p-4 bg-[#f0f4f9] rounded-[12px] text-[#1f1f1f]">
+              <div>
+                <h1 className="text-[16px]  font-medium">Contact Details</h1>
+              </div>
+              <div className="flex items-start justify-start space-x-3">
+                <div>
+                  <MdOutlineEmail size={20} />
+                </div>
+                <div className="">
+                  <p className="break-all">
+                    abdullahbinomarchowdhury02@gmail.com
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-start justify-start space-x-3">
+                <div>
+                  <MdOutlineLocalPhone size={20} />
+                </div>
+                <div className="">
+                  <p className="break-all">01937868838</p>
+                </div>
+              </div>
+              <div className="flex items-start justify-start space-x-3">
+                <div>
+                  <MdOutlineLocationOn size={20} />
+                </div>
+                <div className="">
+                  <p className="break-all">
+                    Unus Optics,Khilkhet,Dhaka 1229,BD
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-start justify-start space-x-3">
+                <div>
+                  <MdOutlineCake size={20} />
+                </div>
+                <div className="">
+                  <p className="break-all">04 June 2002</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col space-y-3 mt-5 mb-5 p-4 text-[#1f1f1f] text-[16px]">
             <div>
-              <h1 className="text-[16px]  font-medium">Contact Details</h1>
+              <h1 className="font-medium">History</h1>
             </div>
-            <div className="flex items-start justify-start space-x-3">
-              <div>
-                <MdOutlineEmail size={20} />
-              </div>
-              <div className="">
-                <p className="break-all">
-                  abdullahbinomarchowdhury02@gmail.com
-                </p>
-              </div>
-            </div>
-            <div className="flex items-start justify-start space-x-3">
-              <div>
-                <MdOutlineLocalPhone size={20} />
-              </div>
-              <div className="">
-                <p className="break-all">01937868838</p>
-              </div>
-            </div>
-            <div className="flex items-start justify-start space-x-3">
-              <div>
-                <MdOutlineLocationOn size={20} />
-              </div>
-              <div className="">
-                <p className="break-all">Unus Optics,Khilkhet,Dhaka 1229,BD</p>
-              </div>
-            </div>
-            <div className="flex items-start justify-start space-x-3">
-              <div>
-                <MdOutlineCake size={20} />
-              </div>
-              <div className="">
-                <p className="break-all">04 June 2002</p>
-              </div>
-            </div>
+            <div className="">Last Edited 3 April 2025</div>
+            <div className="">Added To Contacts 1 April 2025</div>
           </div>
-        </div>
-        <div className="flex flex-col space-y-3 mt-5 mb-5 p-4 text-[#1f1f1f] text-[16px]">
-          <div>
-            <h1 className="font-medium">History</h1>
-          </div>
-          <div className="">Last Edited 3 April 2025</div>
-          <div className="">Added To Contacts 1 April 2025</div>
         </div>
       </div>
     </>
