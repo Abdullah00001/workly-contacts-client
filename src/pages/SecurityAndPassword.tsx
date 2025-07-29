bfda4f3c8abc84878ceb99f400763f650dec5354
abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2025-07-29 13:12:34 +0600

    feature(security page): implement security page overview and active sessions sections

--- src/pages/SecurityAndPassword.tsx
+++ src/pages/SecurityAndPassword.tsx
@@ -1,9 +1,23 @@
-import {FC} from 'react'
+import { FC } from 'react';
+import SecurityOverview from '../features/accountscenter/components/SecurityOverview';
+import ActiveSessions from '../features/accountscenter/components/ActiveSessions';
 
-const SecurityAndPassword:FC = () => {
+const SecurityAndPassword: FC = () => {
   return (
-    <div>SecurityAndPassword</div>
-  )
-}
+    <div className="px-4 min-h-screen w-full">
+      <div className="mt-4 w-auto">
+        <h1 className="font-semibold text-[24px] text-wrap">
+          Keep your Account safe and secure
+        </h1>
+        <p className="text-[16px]  text-wrap mt-1 font-normal text-gray-300">
+          Update your password, monitor logged-in devices, and review recent
+          activity to keep your account safe and under your control.
+        </p>
+      </div>
+      <SecurityOverview />
+      <ActiveSessions />
+    </div>
+  );
+};
 
-export default SecurityAndPassword
+export default SecurityAndPassword;
