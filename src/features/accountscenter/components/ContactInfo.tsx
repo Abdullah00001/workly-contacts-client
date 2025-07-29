0b3db409483816de4f0bf04ecd2b19cd2560b578
abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2025-07-29 16:32:17 +0600

    feature(accounts-center):ui upgrade contact info info section

--- src/features/accountscenter/components/ContactInfo.tsx
+++ src/features/accountscenter/components/ContactInfo.tsx
@@ -5,27 +5,35 @@ const ContactInfo: FC = () => {
   return (
     <div className="w-full  mt-4 mb-4 border border-gray-500 p-4 rounded-[8px]">
       <h5 className="font-medium text-[16px]">Contact Info</h5>
-      <div className="flex mt-2 items-center justify-between w-full">
-        <div className="flex flex-col min-[620px]:w-[60%] min-[620px]:flex-row min-[620px]:justify-between min-[620px]:items-center">
-          <h6 className="text-xs font-medium min-[620px]:w-[50%]">Email</h6>
-          <h5 className="text-[16px] font-normal mt-1 min-[620px]:w-[50%]">
-            abdullahbinomarchowdhury02@gmail.com
-          </h5>
+      <div className="mt-2">
+        <div className="flex transition-all duration-300 lg:cursor-pointer lg:hover:bg-gray-50/5 lg:p-2 lg:border-b lg:border-gray-400 items-center justify-between w-full">
+          <div className="flex flex-col min-[620px]:w-[60%] min-[620px]:flex-row min-[620px]:justify-between min-[620px]:items-center">
+            <h6 className="text-xs font-medium min-[620px]:w-[50%]">Email</h6>
+            <h5 className="text-[16px] font-normal mt-1 min-[620px]:w-[50%]  whitespace-nowrap">
+              jhondoe@gmail.com
+            </h5>
+          </div>
+          <div className="flex justify-end min-[620px]:w-[40%]">
+            <ChevronRight
+              size={25}
+              className="transition-transform duration-200 hover:translate-x-1 text-gray-400"
+            />
+          </div>
         </div>
-        <div className="flex justify-end min-[620px]:w-[40%]">
-          <ChevronRight size={25} className=" text-gray-400" />
-        </div>
-      </div>
-      <hr className="mt-3 mb-3 text-gray-400" />
-      <div className="flex items-center justify-between w-full">
-        <div className="flex flex-col min-[620px]:w-[60%] min-[620px]:flex-row min-[620px]:justify-between min-[620px]:items-center">
-          <h6 className="text-xs font-medium min-[620px]:w-[50%]">Phone</h6>
-          <h5 className="text-[16px] font-normal mt-1 min-[620px]:w-[50%]">
-            01937868838
-          </h5>
-        </div>
-        <div className="flex justify-end min-[620px]:w-[40%]">
-          <ChevronRight size={25} className=" text-gray-400" />
+        <hr className="lg:hidden mt-3 mb-3 text-gray-400" />
+        <div className="flex transition-all duration-300 lg:cursor-pointer lg:hover:bg-gray-50/5 lg:p-2 items-center justify-between w-full">
+          <div className="flex flex-col min-[620px]:w-[60%] min-[620px]:flex-row min-[620px]:justify-between min-[620px]:items-center">
+            <h6 className="text-xs font-medium min-[620px]:w-[50%]">Phone</h6>
+            <h5 className="text-[16px] font-normal mt-1 min-[620px]:w-[50%]">
+              01988888888
+            </h5>
+          </div>
+          <div className="flex justify-end min-[620px]:w-[40%]">
+            <ChevronRight
+              size={25}
+              className="transition-transform duration-200 hover:translate-x-1 text-gray-400"
+            />
+          </div>
         </div>
       </div>
     </div>
