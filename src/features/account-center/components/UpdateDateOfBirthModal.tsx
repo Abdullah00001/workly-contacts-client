6d9a110155135f54207df2c2566cfefaf55efe2c
abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2025-10-07 15:32:26 +0600

    feature(account-center): date of birth change modal api integration complete

--- src/features/account-center/components/UpdateDateOfBirthModal.tsx
+++ src/features/account-center/components/UpdateDateOfBirthModal.tsx
@@ -12,13 +12,49 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { FC, useEffect, useState } from 'react';
-import { TUpdateDateOfBirthModalProps } from '../types/personal-info-types';
+import {
+  TProfileUpdatePayload,
+  TUpdateDateOfBirthModalProps,
+} from '../types/personal-info-types';
+import { UpdateProfileField } from '../services/personal-info-services';
+import { toast } from 'sonner';
+import { AxiosResponse } from 'axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
 const UpdateDateOfBirthModal: FC<TUpdateDateOfBirthModalProps> = ({
   isUpdateDateOfBirthModalOpen,
   dateOfBirth,
   setIsUpdateDateOfBirthModalOpen,
 }) => {
+  const queryClient = useQueryClient();
   const [userDob, setUserDob] = useState<string>('');
+  const { isPending, mutate } = useMutation({
+    mutationFn: async (payload: TProfileUpdatePayload) =>
+      await UpdateProfileField(payload),
+    onSuccess: (data) => {
+      queryClient.setQueryData(['personal_info'], (oldData: AxiosResponse) => {
+        if (!oldData) return oldData;
+        return {
+          ...oldData,
+          dateOfBirth: data.dateOfBirth,
+        };
+      });
+      toast.success('Date Of Birth Changed', { closeButton: false });
+      setIsUpdateDateOfBirthModalOpen(false);
+    },
+    onError: (error) => {
+      console.log(error.message);
+      toast.error('Date Of Birth Change Failed,Try Again!', {
+        closeButton: false,
+      });
+    },
+  });
+  const isFieldChange = dateOfBirth === userDob;
+  const handleSave = () => {
+    if (!isFieldChange) {
+      mutate({ dateOfBirth: userDob });
+    }
+  };
   useEffect(() => {
     setUserDob(dateOfBirth ?? '');
   }, [dateOfBirth]);
@@ -48,7 +84,13 @@ const UpdateDateOfBirthModal: FC<TUpdateDateOfBirthModalProps> = ({
           <DialogClose asChild>
             <Button variant="outline">Cancel</Button>
           </DialogClose>
-          <Button type="submit">Save changes</Button>
+          <Button
+            onClick={handleSave}
+            disabled={isFieldChange || isPending}
+            type="submit"
+          >
+            {isPending ? 'Processing...' : 'Save changes'}
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
